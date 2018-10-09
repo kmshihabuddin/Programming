884837f70c5066e09b7bddfde05afebc0ade1af5
K M Shihab Uddin <kmshihabuddin@gmail.com>
2018-10-09 16:25:41 -0500

    edits

--- Data Structures/segmentTree.cpp
+++ Data Structures/segmentTree.cpp
@@ -5,6 +5,7 @@
 segmentTree::segmentTree(std::vector<int>& a):dummyValue(INT_MAX){
 	nodes = nextPowerOf2((int)a.size());
 	tree = std::vector<int>(2*nodes-1, dummyValue);
+	lazyTree = std::vector<int>(2 * nodes - 1, 0);
 	std::copy(a.begin(), a.end(), tree.begin() + nodes - 1);
 
 	for (int i = nodes - 2; i >= 0; --i){
@@ -14,6 +15,10 @@ segmentTree::segmentTree(std::vector<int>& a):dummyValue(INT_MAX){
 
 int segmentTree::rangeQuery(int left, int right){
 	return rangeQuery(left, right, 0, nodes - 1, 0);
+}
+
+void segmentTree::rangeUpdate(int left, int right, int valueAdd) {
+	rangeUpdate(left, right, valueAdd, 0, nodes - 1, 0);
 }
 
 void segmentTree::update(int index, int value){
@@ -33,17 +38,60 @@ void segmentTree::update(int index, int value){
 	
 }
 
+
+
 int segmentTree::rangeQuery(int left, int right, int left_ind, int right_ind, int pos){
 	if (right<left_ind || left>right_ind){
 		return dummyValue;
 	}
 	else if (left_ind >= left && right_ind <= right){
+		if (lazyTree[pos] == 0) {
+			return tree[pos];
+		}
+		tree[pos] += lazyTree[pos];
+		if (2 * pos + 2 < 2 * nodes - 1) {
+			lazyTree[2 * pos + 1] = lazyTree[pos];
+			lazyTree[2 * pos + 2] = lazyTree[pos];
+		}
+		lazyTree[pos] = 0;
 		return tree[pos];
 	}
 	else{
+		tree[pos] += lazyTree[pos];
+		if (2 * pos + 2 < 2 * nodes - 1) {
+			lazyTree[2 * pos + 1] = lazyTree[pos];
+			lazyTree[2 * pos + 2] = lazyTree[pos];
+		}
+		lazyTree[pos] = 0;
 		int mid_ind = (left_ind + right_ind) / 2;
 		return std::min(rangeQuery(left, right, left_ind, mid_ind, 2 * pos + 1), 
 						rangeQuery(left, right, mid_ind + 1, right_ind, 2 * pos + 2));
+	}
+}
+
+
+
+int segmentTree::rangeUpdate(int left, int right, int valueAdd, int left_ind, int right_ind, int pos) {
+	if (left_ind > right || right_ind < left) {
+		return dummyValue;
+	}
+	else if (left_ind >= left && right_ind <= right) {
+		tree[pos] += valueAdd;
+		if (2 * pos + 2 < 2 * nodes - 1) {
+			lazyTree[2 * pos + 1] += valueAdd;
+			lazyTree[2 * pos + 2] += valueAdd;
+		}
+		return tree[pos];
+	}
+	else {
+		tree[pos] += valueAdd;
+		if (2 * pos + 2 < 2 * nodes - 1) {
+			lazyTree[2 * pos + 1] += valueAdd;
+			lazyTree[2 * pos + 2] += valueAdd;
+		}
+		int mid_ind = (left_ind + right_ind) / 2;
+		tree[pos] = std::min(rangeUpdate(left, right, valueAdd, left_ind, mid_ind, 2 * pos + 1),
+							 rangeUpdate(left, right, valueAdd, mid_ind + 1, right_ind, 2 * pos + 2));
 	}
 }
 
--- Data Structures/segmentTree.h
+++ Data Structures/segmentTree.h
@@ -7,14 +7,17 @@
 class segmentTree{
 	int nextPowerOf2(int n);
 	int rangeQuery(int left, int right, int left_ind, int right_ind, int pos);
+	int rangeUpdate(int left, int right, int valueAdd, int left_ind, int right_ind, int pos);
 public:
 	std::vector<int> tree;
+	std::vector<int> lazyTree;
 	int nodes;
 	int dummyValue;
 
 	segmentTree(std::vector<int>& a);//dummy=0 for sum, INT_MAX or min query, INT_MIN for max_query
 	int rangeQuery(int left, int right); //0 based; inclusive
 	void update(int index, int value);
+	void rangeUpdate(int left, int right, int valueAdd);
 };
 
 #endif
