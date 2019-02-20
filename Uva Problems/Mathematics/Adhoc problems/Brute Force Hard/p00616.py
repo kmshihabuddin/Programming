1f58abe11455ec314e96d92e127ccaf71ebb419c
K M Shihab Uddin <kmshihabuddin@gmail.com>
2019-02-20 02:27:58 -0600

    update

--- Uva Problems/Mathematics/Adhoc problems/Brute Force Hard/p00616.py
+++ Uva Problems/Mathematics/Adhoc problems/Brute Force Hard/p00616.py
@@ -6,13 +6,20 @@ Created on Mon Feb 18 22:08:34 2019
 """
 
 def findDivisors(n,divisors):
-    divisors.clear()
     var=2
     while var*var<=n:
         if n%var==0:
-            divisors.insert(var)
+            divisors.append(var)
         var+=1
     return 
+
+def isValid(total,people):
+    for _ in range(people):
+        total-=1
+        if total%people:
+            return False
+        total=(total*(people-1))//people
+    return total%people==0
 
 
 total=int(input())
@@ -20,8 +27,18 @@ total=int(input())
 while total>=0:
     
     divisors=[]
-    findDivisors(total,divisors)
-    print(divisors)
+    findDivisors(total-1,divisors)
+    
+    res=-1
+    for divisor in divisors[::-1]:
+        if isValid(total,divisor):
+            res=divisor
+            break
+    
+    if res==-1:
+        print(str(total)+' coconuts, no solution')
+    else:
+        print(str(total)+' coconuts, '+str(res)+' people and 1 monkey')
     
     total=int(input())
 
